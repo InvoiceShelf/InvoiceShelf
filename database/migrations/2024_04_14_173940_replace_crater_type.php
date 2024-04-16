a4240bf7fab183736b38d16abcacd896b13f3d7a
tbeck <beckmann.timo@icloud.com>
2024-04-16 17:21:52 +0200

    migrate custom_field_values

--- database/migrations/2024_04_14_173940_replace_crater_type.php
+++ database/migrations/2024_04_14_173940_replace_crater_type.php
@@ -15,6 +15,7 @@ return new class extends Migration
             DB::update("UPDATE email_logs SET mailable_type = REPLACE(model_type, 'Crater', 'InvoiceShelf')");
             DB::update("UPDATE notifications SET notifiable_type = REPLACE(notifiable_type, 'Crater', 'InvoiceShelf')");
             DB::update("UPDATE personal_access_tokens SET tokenable_type = REPLACE(tokenable_type, 'Crater', 'InvoiceShelf')");
+            DB::update("UPDATE custom_field_values SET custom_field_valuable_type = REPLACE(custom_field_valuable_type, 'Crater', 'InvoiceShelf')");
         } catch (\Exception $e) {
         }
     }
@@ -29,6 +30,7 @@ return new class extends Migration
             DB::update("UPDATE email_logs SET mailable_type = REPLACE(model_type, 'InvoiceShelf', 'Crater')");
             DB::update("UPDATE notifications SET notifiable_type = REPLACE(notifiable_type, 'InvoiceShelf', 'Crater')");
             DB::update("UPDATE personal_access_tokens SET tokenable_type = REPLACE(tokenable_type, 'InvoiceShelf', 'Crater')");
+            DB::update("UPDATE custom_field_values SET custom_field_valuable_type = REPLACE(custom_field_valuable_type, 'InvoiceShelf', 'Crater')");
         } catch (\Exception $e) {
         }
     }
