dbf9cbdbb211657a5a91ffa60e641dc8e541451f
Stefan Azarić <9116773+azaricstefan@users.noreply.github.com>
2024-05-23 00:41:44 +0200

    Create 2024_05_22_223547_replace_crater_custom_fields.php
    Fix custom fields

---
+++ database/migrations/2024_05_22_223547_replace_crater_custom_fields.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::update("UPDATE custom_field_values SET custom_field_valuable_type = REPLACE(custom_field_valuable_type, 'Crater', 'InvoiceShelf')");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::update("UPDATE custom_field_values SET custom_field_valuable_type = REPLACE(custom_field_valuable_type, 'InvoiceShelf', 'Crater')");
+    }
+};
