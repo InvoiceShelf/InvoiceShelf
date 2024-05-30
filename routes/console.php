a35df7a4ddb4e9eedb3a29039813572d435bcff0
Shift <shift@laravelshift.com>
2024-05-30 12:18:43 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,29 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use InvoiceShelf\Models\CompanySetting;
+use InvoiceShelf\Models\RecurringInvoice;
+use InvoiceShelf\Space\InstallUtils;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+if (InstallUtils::isDbCreated()) {
+    Schedule::command('check:invoices:status')
+        ->daily();
+
+    Schedule::command('check:estimates:status')
+        ->daily();
+
+    $recurringInvoices = RecurringInvoice::where('status', 'ACTIVE')->get();
+    foreach ($recurringInvoices as $recurringInvoice) {
+        $timeZone = CompanySetting::getSetting('time_zone', $recurringInvoice->company_id);
+
+        Schedule::call(function () use ($recurringInvoice) {
+            $recurringInvoice->generateInvoice();
+        })->cron($recurringInvoice->frequency)->timezone($timeZone);
+    }
+}
