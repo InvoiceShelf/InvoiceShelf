314abe4cc6c4240fbe331318911f0aa16696f144
Darko Gjorgjijoski <dg@darkog.com>
2024-07-28 19:54:07 +0200

    Add node and exif & update entrypoint

--- .dev/php/entrypoint.sh
+++ .dev/php/entrypoint.sh
@@ -4,9 +4,30 @@ echo "############################################"
 echo "### InvoiceShelf Development Environment ###"
 echo "############################################"
 
-chown -R ${UID}:${GID} /home/invoiceshelf/app
-chmod +x /home/invoiceshelf/app/artisan
+cd /home/invoiceshelf/app
 
-cd /home/invoiceshelf/app && php artisan storage:link
+chmod 775 storage/framework
+chmod 775 storage/logs
+chmod 775 bootstrap/cache
+
+chown -R ${UID}:${GID} /home/invoiceshelf/app
+
+chmod +x artisan
+
+if [ ! -d vendor ]; then
+    composer install
+fi
+
+if [ ! -f .env ]; then
+    cp .env.example .env
+    php artisan key:generate --force
+fi
+
+if [ ! -d node_modules ]; then
+    npm install
+    npm run build
+fi
+
+php artisan storage:link
 
 exec $@
