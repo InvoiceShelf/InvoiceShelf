6422da193be26c75f15041d6661ed7063ea358f6
Darko Gjorgjijoski <dg@darkog.com>
2024-07-29 13:01:43 +0200

    Improve .dev/php/entrypoint permissions setting

--- .dev/php/entrypoint.sh
+++ .dev/php/entrypoint.sh
@@ -6,32 +6,39 @@ echo "############################################"
 
 cd /home/invoiceshelf/app
 
-chmod 775 storage/framework
-chmod 775 storage/logs
-chmod 775 bootstrap/cache
 
-chown -R ${UID}:${GID} /home/invoiceshelf/app
-
-chmod +x artisan
-
+# Composer build
 if [ ! -d vendor ]; then
     composer install
 fi
 
+# Empty sqlite database
 if [ ! -f database/database.sqlite ]; then
     cp database/stubs/sqlite.empty.db database/database.sqlite
 fi
 
+# .env file set up
 if [ ! -f .env ]; then
     cp .env.example .env
     php artisan key:generate --force
 fi
 
+# NPM build
 if [ ! -d node_modules ]; then
     npm install
     npm run build
 fi
 
+# Storage symlink
 php artisan storage:link
+
+# Permissions
+chmod 775 storage/framework
+chmod 775 storage/logs
+chmod 775 bootstrap/cache
+chown -R ${UID}:${GID} /home/invoiceshelf/app
+chmod +x artisan
+
+echo "Entrypoint complete."
 
 exec $@
