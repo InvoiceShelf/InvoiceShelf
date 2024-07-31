f10154539ed3c99b6238297be5808bbe608ba31e
jacobi petrucciani <j@cobi.dev>
2024-07-31 14:22:48 -0400

    fix logo rendering in pdf output for invoices

--- resources/views/app/pdf/invoice/invoice1.blade.php
+++ resources/views/app/pdf/invoice/invoice1.blade.php
@@ -339,7 +339,7 @@
             <tr>
                 <td class="text-center">
                     @if ($logo)
-                        <img class="header-logo" style="height:50px" src="{{ $logo }}" alt="Company Logo">
+                        <img class="header-logo" style="height:50px" src="{{ "data:".mime_content_type($logo).";base64,".base64_encode(file_get_contents($logo)) }}" alt="Company Logo">
                     @else
                         @if ($invoice->customer->company)
                             <h2 class="header-logo"> {{ $invoice->customer->company->name }}</h2>
--- resources/views/app/pdf/invoice/invoice2.blade.php
+++ resources/views/app/pdf/invoice/invoice2.blade.php
@@ -389,7 +389,7 @@
             <tr>
                 <td width="60%" class="header-section-left">
                     @if ($logo)
-                        <img class="header-logo" src="{{ $logo }}" alt="Company Logo" style="height: 50px;">
+                        <img class="header-logo" src="{{ "data:".mime_content_type($logo).";base64,".base64_encode(file_get_contents($logo)) }}" alt="Company Logo" style="height: 50px;">
                     @elseif ($invoice->customer->company)
                         <h1 class="header-logo" style="padding-top: 0px;">
                             {{ $invoice->customer->company->name }}
