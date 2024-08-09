5a38e56a3194e3b635bd5a28cbd09a9a525f86f0
troky <troky2001@yahoo.com>
2024-08-09 09:04:28 +0200

    Changed invoice date to datetime

---
+++ app/Http/Controllers/V1/Admin/General/TimeFormatsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace InvoiceShelf\Http\Controllers\V1\Admin\General;
+
+use InvoiceShelf\Http\Controllers\Controller;
+use InvoiceShelf\Space\TimeFormatter;
+use Illuminate\Http\Request;
+
+class TimeFormatsController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        return response()->json([
+            'time_formats' => TimeFormatter::get_list(),
+        ]);
+    }
+}
