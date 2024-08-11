eff67be66085d1a820418816399ed28db29d842b
mchev <martin.chevignard@gmail.com>
2024-08-11 08:38:25 +0200

    Using Space Environment Manager updateEnv method

--- app/Console/Commands/UpdateDotEnv.php
+++ app/Console/Commands/UpdateDotEnv.php
@@ -2,10 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Space\EnvironmentManager;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Str;
 
 class UpdateDotEnv extends Command
 {
@@ -28,27 +27,23 @@ class UpdateDotEnv extends Command
      */
     public function handle()
     {
-        try {
-            $this->updateEnvFile([
-                'APP_TIMEZONE' => config('app.timezone') ?? 'UTC',
+        $environmentManager = new EnvironmentManager();
 
+        try {
+            $environmentManager->updateEnv([
+                'APP_TIMEZONE' => config('app.timezone') ?? 'UTC',
                 'APP_LOCALE' => config('app.locale') ?? 'en',
                 'APP_FALLBACK_LOCALE' => config('app.fallback_locale') ?? 'en',
                 'APP_FAKER_LOCALE' => config('app.faker_locale') ?? 'en_US',
-
                 'LOG_STACK' => 'single',
-
                 'SESSION_ENCRYPT' => 'false',
                 'SESSION_PATH' => '/',
                 'SESSION_DOMAIN' => 'null',
-
                 'BROADCAST_CONNECTION' => 'log',
                 'FILESYSTEM_DISK' => 'local',
                 'QUEUE_CONNECTION' => 'sync',
-
                 'CACHE_STORE' => 'file',
                 'CACHE_PREFIX' => '',
-
                 'REDIS_CLIENT' => 'phpredis',
             ]);
 
@@ -61,54 +56,5 @@ class UpdateDotEnv extends Command
 
         $this->info('Configuration cached successfully.');
 
-    }
-
-    /**
-     * Update the .env file with the provided data.
-     *
-     * @throws \Exception
-     */
-    private function updateEnvFile(array $data): void
-    {
-        $envFile = base_path('.env');
-
-        if (! File::exists($envFile)) {
-            throw new \Exception('The .env file does not exist.');
-        }
-
-        // Read the contents of the .env file
-        $contents = File::get($envFile);
-
-        // Split the contents into an array of lines
-        $lines = explode("\n", $contents);
-
-        // Loop through the lines and update the values
-        foreach ($lines as &$line) {
-            if (empty($line) || Str::startsWith($line, '#')) {
-                continue;
-            }
-
-            // Split each line into key and value
-            $parts = explode('=', $line, 2);
-            $key = $parts[0];
-
-            // Check if the key exists in the provided data
-            if (isset($data[$key])) {
-                // Update the value
-                $line = $key.'='.$data[$key];
-                unset($data[$key]);
-            }
-        }
-
-        // Append any new keys that were not present in the original file
-        foreach ($data as $key => $value) {
-            $lines[] = $key.'='.$value;
-        }
-
-        // Combine the lines back into a string
-        $updatedContents = implode("\n", $lines);
-
-        // Write the updated contents back to the .env file
-        File::put($envFile, $updatedContents);
     }
 }
