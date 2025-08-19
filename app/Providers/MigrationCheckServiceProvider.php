22d49512609fbc522fde37d611570be759d94f46
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-08-19 09:59:39 +0000

    feat: create a migration provider
    Whose sole responsibility is to ensure migration status are consistent with apps code version

---
+++ app/Providers/MigrationCheckServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\View;
+
+class MigrationCheckServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            return;
+        }
+
+        if (!$this->areMigrationsUpToDate()) {
+            $this->showMigrationWarningPage();
+        }
+    }
+
+    /**
+     * Check if migrations are up to date.
+     *
+     * @return bool
+     */
+    protected function areMigrationsUpToDate()
+    {
+        $migrated = DB::table('migrations')->count();
+        $totalMigrations = count(\File::files(database_path('migrations')));
+
+        return $migrated === $totalMigrations;
+    }
+
+    /**
+     * Show a custom migration warning page.
+     *
+     * @return void
+     */
+    protected function showMigrationWarningPage()
+    {
+        $view = View::make('migrations.warning');
+
+        echo $view->render();
+        exit;
+    }
+}
