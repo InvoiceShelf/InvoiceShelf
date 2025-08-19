d9b3fb53d9f5632d881a65b23210c81fbdfb1ef0
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-08-19 09:58:03 +0000

    feat: register a view to display migration error screen

---
+++ resources/views/migrations/warning.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Migrations Out of Date</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding: 50px;
+        }
+
+        h1 {
+            color: #d9534f;
+        }
+
+        code {
+            padding: 2px 4px;
+            border-radius: 5px;
+            background: black;
+            color: white
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Migrations Are Not Up to Date</h1>
+    <p>Please run <code>$ php artisan migrate</code> to update the database.</p>
+</body>
+
+</html>
