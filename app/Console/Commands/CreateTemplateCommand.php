d5c15589f50b71703f845a0ac4529170d159add3
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-08-19 05:23:39 +0000

    refactor(cli/createTemplate):  update CreateTemplateCommand for improved template handling
    - Updated command signature to use options for both name and type.
    - Refactored handle method to improve readability and maintainability.
    - Introduced helper methods for getting options, checking template existence, and cloning templates.
    - Enhanced error messages for better user guidance.
    - Improved structure for handling template partials and thumbnails.

--- app/Console/Commands/CreateTemplateCommand.php
+++ app/Console/Commands/CreateTemplateCommand.php
@@ -15,7 +15,9 @@ class CreateTemplateCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'make:template {name} {--type=}';
+    protected $signature = 'make:template
+                            {--N|name= : Template Name e.g. standard}
+                            {--T|type= : Template Type [invoice | estimate]}';
 
     /**
      * The console command description.
@@ -36,58 +38,284 @@ class CreateTemplateCommand extends Command
 
     /**
      * Execute the console command.
+     *
+     * @return int
      */
-    public function handle(): int
+    public function handle()
     {
-        $templateName = $this->argument('name');
-        $templateType = $this->option('type');
+        /**
+         * Template Name
+         *
+         * @var array
+         */
+        $name = $this->getNameOption();
+        /**
+         * Template Type
+         *
+         * @var string
+         */
+        $type = $this->getTypeOption();
 
-        if (! $templateType) {
-            $templateType = $this->choice('Create a template for?', ['invoice', 'estimate']);
-        }
-
-        if (PdfTemplateUtils::customTemplateFileExists($templateType, sprintf('%s.blade.php', $templateName))) {
-            $this->info('Template with given name already exists.');
+        if ($this->isTemplateNew($type, $name)) {
+            $this->info('- Template with given name already exists.');
 
             return self::INVALID;
         }
 
-        if (! PdfTemplateUtils::toCustomTemplateMarkupFile(
-            Str::replace(
-                sprintf('app.pdf.%s', $templateType),
-                sprintf('pdf_templates::%s', $templateType),
-                Storage::disk('views')->get("/app/pdf/{$templateType}/{$templateType}1.blade.php"),
-            ),
-            $templateType,
-            $templateName
-        )) {
-            $this->error(sprintf('Unable to create %s template.', ucfirst($templateType)));
+        if (! $this->cloneTemplate($type, $name)) {
+            $this->error(
+                sprintf('Unable to create %s template.', ucfirst($type))
+            );
+            $this->info('make sure template name is free of special '
+                . 'characters.');
 
             return self::FAILURE;
         }
 
-        PdfTemplateUtils::toCustomTemplateImageFile(
-            File::get(resource_path("static/img/PDF/{$templateType}1.png")),
-            $templateType,
-            $templateName,
-        );
+        if (! $this->cloneTemplateThumbnail($type, $name)) {
+            $this->error(sprintf(
+                'Unable to clone %s template thumbnail from default template.',
+                ucfirst($type)
+            ));
 
-        if (! PdfTemplateUtils::customTemplateFileExists($templateType, 'partials/table.blade.php')) {
-            PdfTemplateUtils::toCustomTemplateFile(
-                Storage::disk('views')->get("/app/pdf/{$templateType}/partials/table.blade.php"),
-                $templateType,
+            return self::FAILURE;
+        }
+
+        if ($this->templateHasPartials($type, 'partials/table.blade.php')) {
+            if (! $this->cloneTemplatePartials(
+                $type,
                 'partials/table.blade.php'
-            );
+            )) {
+                $this->error(sprintf(
+                    'Unable to clone %s template partials from default'
+                        . ' template.',
+                    ucfirst($type)
+                ));
+
+                return self::FAILURE;
+            }
         }
 
         $this->info(
+            sprintf('%s Template created successfully', ucfirst($type))
+        );
+        $this->info(sprintf(
+            'you will find it at in: %s',
+            PdfTemplateUtils::getCustomTemplateFilePath(
+                $type,
+                sprintf('%s.blade.php', $name)
+            )
+        ));
+
+        return self::SUCCESS;
+    }
+
+    /**
+     * Check if option is valid type
+     *
+     * @param array|string|bool|null $option command option
+     *
+     * @return bool
+     */
+    protected function validOption($option)
+    {
+        return !is_bool($option)
+            && !is_array($option)
+            && !is_null($option)
+            && !empty($option);
+    }
+
+    /**
+     * Get template name
+     *
+     * @return string
+     */
+    protected function getNameOption()
+    {
+        $name = strtolower($this->option('name'));
+
+        if ($this->validOption($name)) {
+            return $name;
+        }
+
+
+        $this->error('Template name missing or invalid.');
+
+        $label = 'What would you like to name the template?';
+
+        // TODO: User Input needs to be properly sanitised.
+        return strval($this->ask($label));
+    }
+
+    /**
+     * Get template type
+     *
+     * @return string
+     */
+    protected function getTypeOption()
+    {
+        $type = strtolower($this->option('type'));
+        $choice = [
+            strtolower(PdfTemplateUtils::INVOICE),
+            strtolower(PdfTemplateUtils::ESTIMATE)
+        ];
+
+        if ($this->validOption($type) && in_array($type, $choice)) {
+            return $type;
+        }
+
+        $this->error('Template type missing or invalid.');
+
+        $label = 'Create a template for?';
+
+        return $this->choice($label, $choice);
+    }
+
+    /**
+     * Check if Template is new
+     *
+     * @param string $type Template Type
+     * @param string $name Template Name
+     *
+     * @return bool
+     */
+    protected function isTemplateNew($type, $name)
+    {
+        $this->info(
+            sprintf('Checking if %s template already exists', ucfirst($type))
+        );
+
+        return PdfTemplateUtils::customTemplateFileExists(
+            $type,
+            sprintf('%s.blade.php', $name)
+        );
+    }
+
+    /**
+     * Clone Template Type
+     *
+     * @param string $type Template Type
+     * @param string $name Template Name
+     *
+     * @return bool Operation Success Status
+     */
+    protected function cloneTemplate($type, $name)
+    {
+        $this->info(sprintf('Cloning default %s template', ucfirst($type)));
+
+        /**
+         * Default template path
+         *
+         * @var string
+         */
+        $oldPath  = sprintf('app.pdf.%s', $type);
+        /**
+         * New template path
+         *
+         * @var string
+         */
+        $newPath = sprintf('pdf_templates::%s', $type);
+        /**
+         * Default template contents
+         *
+         * @var string
+         */
+        $defaultTemplate = Storage::disk('views')->get(
+            "/app/pdf/{$type}/{$type}1.blade.php"
+        );
+        /**
+         * New template contents with all references to old partials @include
+         * paths to new location
+         *
+         * @var string
+         */
+        $content = Str::replace($oldPath, $newPath, $defaultTemplate);
+
+        return PdfTemplateUtils::toCustomTemplateMarkupFile(
+            $content,
+            $type,
+            $name
+        );
+    }
+
+    /**
+     * Clone Template Thumbnail
+     *
+     * @param string $type Template Type
+     * @param string $name Template Name
+     *
+     * @return bool Operation Success Status
+     */
+    protected function cloneTemplateThumbnail($type, $name)
+    {
+        $this->info(
+            sprintf('Cloning default %s template thumbnail', ucfirst($type))
+        );
+
+        /**
+         * Default template thumbnail contents
+         *
+         * @var string
+         */
+        $defaultTemplateThumbContents = File::get(
+            resource_path("static/img/PDF/{$type}1.png")
+        );
+
+        return PdfTemplateUtils::toCustomTemplateImageFile(
+            $defaultTemplateThumbContents,
+            $type,
+            $name,
+        );
+    }
+
+    /**
+     * Check if template type has partials
+     *
+     * @param string $type Template Type
+     * @param string $partialPath Template partial path
+     *
+     * @return bool has partials
+     */
+    protected function templateHasPartials($type, $partialPath)
+    {
+        $this->info(
+            sprintf('Checking if %s template has partials', ucfirst($type))
+        );
+
+        return Storage::disk('views')
+            ->exists(sprintf('app/pdf/%s/%s', $type, $partialPath));
+    }
+
+    /**
+     * Check if template type has partials
+     *
+     * @param string $type Template Type
+     * @param string $partialPath Template partial path
+     *
+     * @return bool Operation Success Status
+     */
+    protected function cloneTemplatePartials($type, $partialPath)
+    {
+        $this->info(
             sprintf(
-                '%s Template created successfully at %s',
-                ucfirst($templateType),
-                PdfTemplateUtils::getCustomTemplateFilePath($templateType, sprintf('%s.blade.php', $templateName))
+                '- %s Template has partials, cloning them as well',
+                ucfirst($type)
             )
         );
 
-        return self::SUCCESS;
+        /**
+         * Default template thumbnail contents
+         *
+         * @var string
+         */
+        $defaultTemplatePartialContents = Storage::disk('views')->get(
+            "/app/pdf/{$type}/{$partialPath}"
+        );
+
+        return PdfTemplateUtils::toCustomTemplateFile(
+            $defaultTemplatePartialContents,
+            $type,
+            $partialPath
+        );
     }
 }
