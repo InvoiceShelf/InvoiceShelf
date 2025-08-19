6d717627291b850ebe71ceb977906e8d3ae16519
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-08-19 00:32:55 +0000

    refactor(cli/estimateStatus): rename $estimates to $staleEstimates for clarity

--- app/Console/Commands/CheckEstimateStatus.php
+++ app/Console/Commands/CheckEstimateStatus.php
@@ -41,9 +41,9 @@ class CheckEstimateStatus extends Command
     {
         $date = Carbon::now();
         $status = [Estimate::STATUS_ACCEPTED, Estimate::STATUS_REJECTED, Estimate::STATUS_EXPIRED];
-        $estimates = Estimate::whereNotIn('status', $status)->whereDate('expiry_date', '<', $date)->get();
+        $staleEstimates = Estimate::whereNotIn('status', $status)->whereDate('expiry_date', '<', $date)->get();
 
-        foreach ($estimates as $estimate) {
+        foreach ($staleEstimates as $estimate) {
             $estimate->status = Estimate::STATUS_EXPIRED;
             printf("Estimate %s is EXPIRED \n", $estimate->estimate_number);
             $estimate->save();
