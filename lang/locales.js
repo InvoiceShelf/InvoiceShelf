399ceede183ec506d36933b95e4c9603f6748211
Darko Gjorgjijoski <dg@darkog.com>
2025-08-28 12:52:17 +0200

    Dynamically load language files

--- lang/locales.js
+++ lang/locales.js
@@ -1,43 +1,7 @@
-import cs from './cs.json'
 import en from './en.json'
-import fr from './fr.json'
-import es from './es.json'
-import ar from './ar.json'
-import de from './de.json'
-import ja from './ja.json'
-import pl from './pl.json'
-import pt_BR from './pt-br.json'
-import it from './it.json'
-import sr from './sr.json'
-import nl from './nl.json'
-import ko from './ko.json'
-import lv from './lv.json'
-import sv from './sv.json'
-import sk from './sk.json'
-import vi from './vi.json'
-import el from './el.json'
-import hr from './hr.json'
-import th from './th.json'
 
+// Only load English by default to reduce initial bundle size
+// Other languages will be loaded dynamically when needed
 export default {
-  cs,
-  en,
-  fr,
-  es,
-  ar,
-  de,
-  ja,
-  pt_BR,
-  it,
-  sr,
-  nl,
-  ko,
-  lv,
-  sv,
-  sk,
-  vi,
-  pl,
-  el,
-  hr,
-  th
+  en
 }
