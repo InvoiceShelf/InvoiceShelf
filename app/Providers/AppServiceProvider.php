7a1d9312bbd40afdbf9fa9791346496dd2cee104
TrueBankai416 <97103466+TrueBankai416@users.noreply.github.com>
2025-11-02 21:21:21 -0500

    Update AppServiceProvider.php
    Add force HTTPS logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -52,7 +52,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
+        
+        if (config('app.force_https')) {
+            \Illuminate\Support\Facades\URL::forceScheme("https");
+        }
+        
         if (InstallUtils::isDbCreated()) {
             $this->addMenus();
         }
